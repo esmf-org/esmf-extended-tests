0dc65104c90d72cd041223755b6004341356498c
Ryan.O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2020-08-18 17:07:36 -0700

    pass click parameters consistently, bash braces, renaming, update Darwin build parameters

--- ESMF_CompWeights/config/Cheyenne.py
+++ ESMF_CompWeights/config/Cheyenne.py
@@ -1,5 +1,16 @@
 import os
 
 RUNDIR="/glade/work/rokuingh/MBMeshCompWeightsResults"
-SRCDIR="/glade/work/rokuingh/sandbox/extended-tests/ESMF_MOABCheck"
+SRCDIR="/glade/work/rokuingh/sandbox/extended-tests/ESMF_CompWeights"
 RegridTestData="RegridTestData.py"
+
+esmf_env = dict(ESMF_OS = "Linux",
+                ESMF_COMPILER = "intel",
+                ESMF_COMM = "mpt",
+                ESMF_NETCDF = "split",
+                ESMF_NETCDF_INCLUDE="/glade/u/apps/ch/opt/netcdf/4.7.1/intel/18.0.5/include",
+                ESMF_NETCDF_LIBPATH="/glade/u/apps/ch/opt/netcdf/4.7.1/intel/18.0.5/lib",
+                ESMF_BOPT="O",
+                ESMF_OPTLEVEL=2,
+                ESMF_ABI=64,
+                ESMF_BUILD_NP=36)
--- ESMF_CompWeights/config/Darwin.py
+++ ESMF_CompWeights/config/Darwin.py
@@ -1,5 +1,17 @@
 import os
 
-RUNDIR="/home/ryan/MBMeshCompWeightsResults"
-SRCDIR="/home/ryan/Dropbox/sandbox/extended-tests/ESMF_MOABCheck"
+RUNDIR="/Users/oehmke/MBMeshCompWeightsResults"
+SRCDIR="/Users/oehmke/sandbox/extended-tests/ESMF_CompWeights"
 RegridTestData="RegridTestData.py"
+
+esmf_env = dict(ESMF_OS = "Darwin",
+                ESMF_COMPILER = "gfortran",
+                ESMF_COMM = "openmpi",
+                ESMF_NETCDF = "split",
+                ESMF_NETCDF_INCLUDE="/usr/local/include",
+                ESMF_NETCDF_LIBPATH="/usr/local/lib",
+                ESMF_BOPT="O",
+                ESMF_OPTLEVEL=1,
+                ESMF_ABI=64,
+                ESMF_BUILD_NP=4)
+
