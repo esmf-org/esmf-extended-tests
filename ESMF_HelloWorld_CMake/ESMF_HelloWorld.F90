0fbf405aabad7d43b9b160578261b58355792960
Gerhard Theurich <theurich@sourcespring.net>
2022-11-18 09:56:02 -0800

    Update to a more recent approach of building ESMF apps with CMake.

--- ESMF_HelloWorld_CMake/ESMF_HelloWorld.F90
+++ ESMF_HelloWorld_CMake/ESMF_HelloWorld.F90
@@ -1,26 +1,27 @@
 ! Earth System Modeling Framework
-! Copyright 2002-2021, University Corporation for Atmospheric Research,
+! Copyright 2002-2022, University Corporation for Atmospheric Research,
 ! Massachusetts Institute of Technology, Geophysical Fluid Dynamics
 ! Laboratory, University of Michigan, National Centers for Environmental
 ! Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
 ! NASA Goddard Space Flight Center.
 ! Licensed under the University of Illinois-NCSA License.
 
-program esmf_application
+program ESMF_HelloWorld
 
   ! modules
   use ESMF
-  
+
   implicit none
-  
+
   ! local variables
-  integer:: rc
-  
+  integer           :: rc
+
   call ESMF_Initialize(rc=rc)
   if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
-  
-  print *, "Hello ESMF World"
 
-  call ESMF_Finalize()
-  
+  call ESMF_LogWrite(">>> Hello ESMF World <<<", rc=rc)
+  if (rc /= ESMF_SUCCESS) call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  call ESMF_Finalize(rc=rc)
+
 end program
